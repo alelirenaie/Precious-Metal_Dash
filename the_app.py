e6708642f41e5146bbe11d7f4cce44ae354111aa
alelirenaie <145812796+alelirenaie@users.noreply.github.com>
2024-01-26 15:14:03 -0700

    Update the_app.py

--- the_app.py
+++ the_app.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 # Read in the data
 data = pd.read_csv("precious_metals_prices_2018_2021.csv")
-data["DateTime"] = pd.to_datetime(data["DateTime"], format="%Y-%m-%d %H:%M:%S")
+data["DateTime"] = pd.to_datetime(data["DateTime"])
 
-# Create a plotly plot for use by st.plotly_chart().
+# Create a default plot using Plotly Express
 fig = px.line(
     data,
     title="Precious Metal Prices 2018-2021",
@@ -16,22 +16,43 @@ fig = px.line(
 )
 
 # Streamlit app layout
-st.set_page_config(page_title="Precious Metal Prices 2018-2021", page_icon=":moneybag:")
-
-# Sidebar
-st.sidebar.title("Options")
-metal_filter = st.sidebar.selectbox("Metal", data.columns[1:], index=1)
-date_range = st.sidebar.date_input("Date Range", [data.DateTime.min().date(), data.DateTime.max().date()])
-
-# Filter data
-filtered_data = data.loc[(data.DateTime >= date_range[0]) & (data.DateTime <= date_range[1])]
+st.set_page_config(
+    page_title="Precious Metal Prices 2018-2021",
+    page_icon="💰"
+)
 
 # Display header
 st.title("Precious Metal Prices")
 st.markdown("The cost of precious metals between 2018 and 2021")
 
+# Sidebar for user inputs
+metal_filter = st.sidebar.selectbox("Select Metal", data.columns[1:], index=1)
+date_range = st.sidebar.date_input("Select Date Range", [data.DateTime.min(), data.DateTime.max()])
+
+# Filter data
+filtered_data = data.loc[(data.DateTime >= date_range[0]) & (data.DateTime <= date_range[1])]
+
 # Display chart
-st.plotly_chart(fig.update_traces(y=[metal_filter], color_discrete_map={"Gold": "gold"}))
+st.plotly_chart(px.line(
+    filtered_data,
+    title=f"Precious Metal Prices 2018-2021 - {metal_filter}",
+    x="DateTime",
+    y=[metal_filter],
+    color_discrete_map={
+        "Platinum": "#B4B8C4",
+        "Gold": "#FFD700",
+        "Silver": "#C0C0C0",
+        "Palladium": "#FF914D",
+        "Rhodium": "#8E9EAB",
+        "Iridium": "#9BF6FF",
+        "Ruthenium": "#D3A297"
+    }
+).update_layout(
+    template="plotly_dark",
+    xaxis_title="Date",
+    yaxis_title=f"Price ({metal_filter} - USD/oz)",
+    font=dict(family="Arial", size=18, color="white")
+))
 
 # Display footer
 st.markdown("Designed with ❤️ by AleliRenaie")
